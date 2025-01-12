884c4defa45c3c4e47e0773b05b0166fe487c8a1
aabccd021 <aabccd021@gmail.com>
2025-01-12 17:38:42 +0700

    change

--- src/index.ts
+++ src/index.ts
@@ -5,232 +5,232 @@ type Scroller = ReturnType<typeof virtua.createScroller>;
 type Resizer = ReturnType<typeof virtua.createResizer>;
 
 interface ChildData {
-	idx: number;
-	hide: boolean;
-	top: string;
-	element: HTMLElement;
-	unobserve: () => void;
+  idx: number;
+  hide: boolean;
+  top: string;
+  element: HTMLElement;
+  unobserve: () => void;
 }
 
 interface State {
-	children: HTMLElement[];
-	childData: ChildData[];
-	containerHeight?: string;
-	jumpCount?: number;
+  children: HTMLElement[];
+  childData: ChildData[];
+  containerHeight?: string;
+  jumpCount?: number;
 }
 
 interface Context {
-	readonly container: HTMLElement;
-	readonly store: virtua.VirtualStore;
-	readonly resizer: Resizer;
-	readonly scroller: Scroller;
-	readonly itemTag?: keyof HTMLElementTagNameMap;
-	readonly state: State;
+  readonly container: HTMLElement;
+  readonly store: virtua.VirtualStore;
+  readonly resizer: Resizer;
+  readonly scroller: Scroller;
+  readonly itemTag?: keyof HTMLElementTagNameMap;
+  readonly state: State;
 }
 
 export function appendChild(context: Context, newChild: HTMLElement[]): void {
-	context.state.children = context.state.children.concat(newChild);
-	context.store.$update(virtua.ACTION_ITEMS_LENGTH_CHANGE, [
-		context.state.children.length,
-		false,
-	]);
-	render(context);
+  context.state.children = context.state.children.concat(newChild);
+  context.store.$update(virtua.ACTION_ITEMS_LENGTH_CHANGE, [
+    context.state.children.length,
+    false,
+  ]);
+  render(context);
 }
 
 function newChild(
-	context: Context,
-	idx: number,
-	top: string,
-	newChildData: ChildData[],
+  context: Context,
+  idx: number,
+  top: string,
+  newChildData: ChildData[],
 ): HTMLElement {
-	const child = context.state.children[idx];
-	if (child === undefined) {
-		throw new Error(`Absurd: child is undefined at index ${idx}`);
-	}
-	const element = document.createElement(context.itemTag ?? "div");
-	element.style.position = "absolute";
-	element.style.visibility = "visible";
-	element.style.top = top;
-	element.style.width = "100%";
-	element.style.left = "0";
-	element.appendChild(child);
-	newChildData.push({
-		idx,
-		hide: false,
-		top,
-		element,
-		unobserve: context.resizer.$observeItem(element, idx),
-	});
+  const child = context.state.children[idx];
+  if (child === undefined) {
+    throw new Error(`Absurd: child is undefined at index ${idx}`);
+  }
+  const element = document.createElement(context.itemTag ?? "div");
+  element.style.position = "absolute";
+  element.style.visibility = "visible";
+  element.style.top = top;
+  element.style.width = "100%";
+  element.style.left = "0";
+  element.appendChild(child);
+  newChildData.push({
+    idx,
+    hide: false,
+    top,
+    element,
+    unobserve: context.resizer.$observeItem(element, idx),
+  });
 
-	return element;
+  return element;
 }
 
 export interface VirtualizerProps {
-	children: HTMLElement[];
-	overscan?: number;
-	itemSize?: number;
-	cache?: virtua.CacheSnapshot;
-	as?: keyof HTMLElementTagNameMap;
-	item?: keyof HTMLElementTagNameMap;
+  children: HTMLElement[];
+  overscan?: number;
+  itemSize?: number;
+  cache?: virtua.CacheSnapshot;
+  as?: keyof HTMLElementTagNameMap;
+  item?: keyof HTMLElementTagNameMap;
 }
 
 interface InitResult {
-	context: Context;
-	dispose: () => void;
-	root: HTMLElement;
+  context: Context;
+  dispose: () => void;
+  root: HTMLElement;
 }
 
 export function init({
-	children,
-	as,
-	itemSize,
-	overscan,
-	cache,
+  children,
+  as,
+  itemSize,
+  overscan,
+  cache,
   item,
 }: VirtualizerProps): InitResult {
-	const container = document.createElement("div");
-	container.style.overflowAnchor = "none";
-	container.style.flex = "none";
-	container.style.position = "relative";
-	container.style.visibility = "hidden";
-	container.style.width = "100%";
+  const container = document.createElement("div");
+  container.style.overflowAnchor = "none";
+  container.style.flex = "none";
+  container.style.position = "relative";
+  container.style.visibility = "hidden";
+  container.style.width = "100%";
 
-	const root = document.createElement(as ?? "div");
-	root.style.display = "block";
-	root.style.overflowY = "auto";
-	root.style.contain = "strict";
-	root.style.width = "100%";
-	root.style.height = "100%";
-	root.appendChild(container);
+  const root = document.createElement(as ?? "div");
+  root.style.display = "block";
+  root.style.overflowY = "auto";
+  root.style.contain = "strict";
+  root.style.width = "100%";
+  root.style.height = "100%";
+  root.appendChild(container);
 
-	const store = virtua.createVirtualStore(
-		children.length,
-		itemSize,
-		overscan,
-		undefined,
-		cache,
-		!itemSize,
-	);
+  const store = virtua.createVirtualStore(
+    children.length,
+    itemSize,
+    overscan,
+    undefined,
+    cache,
+    !itemSize,
+  );
 
-	const resizer = virtua.createResizer(store, false);
-	resizer.$observeRoot(root);
+  const resizer = virtua.createResizer(store, false);
+  resizer.$observeRoot(root);
 
-	const scroller = virtua.createScroller(store, false);
-	scroller.$observe(root);
+  const scroller = virtua.createScroller(store, false);
+  scroller.$observe(root);
 
-	const context: Context = {
-		container,
-		store,
-		resizer,
-		scroller,
+  const context: Context = {
+    container,
+    store,
+    resizer,
+    scroller,
     itemTag: item,
-		state: {
-			childData: [],
-			children,
-		},
-	};
+    state: {
+      childData: [],
+      children,
+    },
+  };
 
-	const unsubscribeStore = store.$subscribe(
-		virtua.UPDATE_VIRTUAL_STATE,
-		(_sync) => {
-			render(context);
-		},
-	);
+  const unsubscribeStore = store.$subscribe(
+    virtua.UPDATE_VIRTUAL_STATE,
+    (_sync) => {
+      render(context);
+    },
+  );
 
-	const dispose = () => {
-		unsubscribeStore();
-		for (const childData of context.state.childData) {
-			childData.unobserve();
-		}
-	};
+  const dispose = () => {
+    unsubscribeStore();
+    for (const childData of context.state.childData) {
+      childData.unobserve();
+    }
+  };
 
-	return { context, dispose, root };
+  return { context, dispose, root };
 }
 
 export function render(context: Context): void {
-	requestAnimationFrame(() => {
-		_render(context);
-	});
+  requestAnimationFrame(() => {
+    _render(context);
+  });
 }
 
 function _render(context: Context): void {
-	const { store, scroller, state, container } = context;
-	const newJumpCount = store.$getJumpCount();
-	if (state.jumpCount !== newJumpCount) {
-		scroller.$fixScrollJump();
-		state.jumpCount = newJumpCount;
-	}
+  const { store, scroller, state, container } = context;
+  const newJumpCount = store.$getJumpCount();
+  if (state.jumpCount !== newJumpCount) {
+    scroller.$fixScrollJump();
+    state.jumpCount = newJumpCount;
+  }
 
-	const newContainerHeight = `${store.$getTotalSize()}px`;
-	if (state.containerHeight !== newContainerHeight) {
-		container.style.height = newContainerHeight;
-		state.containerHeight = newContainerHeight;
-	}
+  const newContainerHeight = `${store.$getTotalSize()}px`;
+  if (state.containerHeight !== newContainerHeight) {
+    container.style.height = newContainerHeight;
+    state.containerHeight = newContainerHeight;
+  }
 
-	const [startIdx, endIdx] = store.$getRange();
-	const newChildData: ChildData[] = [];
-	for (
-		let newChildIdx = startIdx, j = endIdx;
-		newChildIdx <= j;
-		newChildIdx++
-	) {
-		const oldChildDataMaybe: ChildData | undefined = state.childData[0];
-		const top = `${store.$getItemOffset(newChildIdx)}px`;
+  const [startIdx, endIdx] = store.$getRange();
+  const newChildData: ChildData[] = [];
+  for (
+    let newChildIdx = startIdx, j = endIdx;
+    newChildIdx <= j;
+    newChildIdx++
+  ) {
+    const oldChildDataMaybe: ChildData | undefined = state.childData[0];
+    const top = `${store.$getItemOffset(newChildIdx)}px`;
 
-		if (oldChildDataMaybe === undefined) {
-			const childEl = newChild(context, newChildIdx, top, newChildData);
-			container.appendChild(childEl);
-			state.childData.shift();
-			continue;
-		}
+    if (oldChildDataMaybe === undefined) {
+      const childEl = newChild(context, newChildIdx, top, newChildData);
+      container.appendChild(childEl);
+      state.childData.shift();
+      continue;
+    }
 
-		let oldChildData: ChildData = oldChildDataMaybe;
-		while (newChildIdx > oldChildData.idx) {
-			oldChildData.element.remove();
-			oldChildData.unobserve();
-			state.childData.shift();
+    let oldChildData: ChildData = oldChildDataMaybe;
+    while (newChildIdx > oldChildData.idx) {
+      oldChildData.element.remove();
+      oldChildData.unobserve();
+      state.childData.shift();
 
-			const nextOldChild = state.childData[0];
-			if (nextOldChild === undefined) {
-				const childEl = newChild(context, newChildIdx, top, newChildData);
-				container.appendChild(childEl);
-				state.childData.shift();
-				break;
-			}
+      const nextOldChild = state.childData[0];
+      if (nextOldChild === undefined) {
+        const childEl = newChild(context, newChildIdx, top, newChildData);
+        container.appendChild(childEl);
+        state.childData.shift();
+        break;
+      }
 
-			oldChildData = nextOldChild;
-		}
+      oldChildData = nextOldChild;
+    }
 
-		if (newChildIdx < oldChildData.idx) {
-			const childEl = newChild(context, newChildIdx, top, newChildData);
-			container.insertBefore(childEl, oldChildData.element);
-			continue;
-		}
+    if (newChildIdx < oldChildData.idx) {
+      const childEl = newChild(context, newChildIdx, top, newChildData);
+      container.insertBefore(childEl, oldChildData.element);
+      continue;
+    }
 
-		if (oldChildData.idx === newChildIdx) {
-			const prevHide = oldChildData.hide;
-			const hide = store.$isUnmeasuredItem(newChildIdx);
-			if (hide !== prevHide) {
-				oldChildData.element.style.position = hide ? "" : "absolute";
-				oldChildData.element.style.visibility = hide ? "hidden" : "visible";
-				oldChildData.hide = hide;
-			}
+    if (oldChildData.idx === newChildIdx) {
+      const prevHide = oldChildData.hide;
+      const hide = store.$isUnmeasuredItem(newChildIdx);
+      if (hide !== prevHide) {
+        oldChildData.element.style.position = hide ? "" : "absolute";
+        oldChildData.element.style.visibility = hide ? "hidden" : "visible";
+        oldChildData.hide = hide;
+      }
 
-			const prevTop = oldChildData.top;
-			if (top !== prevTop) {
-				oldChildData.element.style.top = top;
-				oldChildData.top = top;
-			}
+      const prevTop = oldChildData.top;
+      if (top !== prevTop) {
+        oldChildData.element.style.top = top;
+        oldChildData.top = top;
+      }
 
-			newChildData.push(oldChildData);
-			state.childData.shift();
-		}
-	}
+      newChildData.push(oldChildData);
+      state.childData.shift();
+    }
+  }
 
-	for (const oldChild of state.childData) {
-		oldChild.element.remove();
-		oldChild.unobserve();
-	}
+  for (const oldChild of state.childData) {
+    oldChild.element.remove();
+    oldChild.unobserve();
+  }
 
-	state.childData = newChildData;
+  state.childData = newChildData;
 }
--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,15 @@ app.style.height = "90dvh";
 document.body.appendChild(app);
 
 function createChild(): HTMLElement {
-	const height = Math.floor(Math.random() * 100) + 20;
-	const el = document.createElement("button");
-	el.textContent = `Height: ${height}px`;
-	el.style.border = "1px solid #ccc";
-	el.style.height = `${height}px`;
+  const height = Math.floor(Math.random() * 100) + 20;
+  const el = document.createElement("button");
+  el.textContent = `Height: ${height}px`;
+  el.style.border = "1px solid #ccc";
+  el.style.height = `${height}px`;
 
-  el.addEventListener("click", () => {
-    console.log("clicked");
-  })
+  el.addEventListener("click", () => {});
 
-	return el;
+  return el;
 }
 
 const children = Array.from({ length: 30 }, createChild);
@@ -26,14 +24,13 @@ const { context, root } = vList.init({ children });
 app.appendChild(root);
 vList.render(context);
 
-
 let count = 0;
 const interval = setInterval(() => {
-	if (count > 10) {
-		clearInterval(interval);
-		return;
-	}
-	const newChildren = Array.from({ length: 10 }, createChild);
-	vList.appendChild(context, newChildren);
-	count++;
+  if (count > 10) {
+    clearInterval(interval);
+    return;
+  }
+  const newChildren = Array.from({ length: 10 }, createChild);
+  vList.appendChild(context, newChildren);
+  count++;
 }, 1000);
