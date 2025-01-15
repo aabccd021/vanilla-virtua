f1a8063c83f924e2bb9e69fe13e8d9b494c75a1d
aabccd021 <aabccd021@gmail.com>
2025-01-15 15:46:47 +0700

    works

--- src/foo.ts
+++ src/foo.ts
@@ -1,16 +1,13 @@
 type Unsubscribe = () => void;
 
 export function init(): Unsubscribe {
-  console.warn("foo.js init");
   const newChild = document.createElement("h2");
   newChild.textContent = "this is dynamically added";
   document.body.appendChild(newChild);
-  console.warn("newChild added");
 
   const newChild2 = document.createElement("h2");
   newChild2.textContent = "only one of these should be shown";
   document.body.appendChild(newChild2);
-  console.warn("newChild2 added");
 
   const h2s = document.querySelectorAll("h2");
   for (const el of h2s) {
@@ -20,7 +17,6 @@ export function init(): Unsubscribe {
   }
 
   return (): void => {
-    console.warn("foo.js unsub");
     newChild2.remove();
   };
 }
