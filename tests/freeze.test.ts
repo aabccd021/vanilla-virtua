203bc7c8604f8fb17884e64cf4bf252cb5221a63
aabccd021 <aabccd021@gmail.com>
2025-01-17 19:26:13 +0700

    Add test cases for string array freezing.

--- tests/freeze.test.ts
+++ tests/freeze.test.ts
@@ -61,6 +61,8 @@ const params: string[][] = [
   ["gi_1", "cs", "bi_1"],
   ["gi_1", "cs", "ci_2", "bs", "bi_1"],
   ["gi_1", "cs", "ci_2", "cs", "bi_1"],
+  ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_1"],
+  ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs", "bi_1"],
 
   [
     "gs",
