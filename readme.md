33590848cda45b485e2edbc46e017889ffe66628
aabccd021 <aabccd021@gmail.com>
2025-01-17 20:43:47 +0700

    1. `Add innerhtml support and non module scripts` 2. `Remove test.ts - Refactor freeze.js` 3. `Update dynamic, increment, static html files` 4. `Modify freeze.test.ts - Add browser launch params`

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Todo
 
+- innerhtml instead of outerhtml
+- support non module scripts
 - assert `/static` is not in sessionStorage
 - only dispatch event unsubscribe
 - set timeout 0 instead of animation frame
--- tests/freeze.test.ts
+++ tests/freeze.test.ts
@@ -37,10 +37,14 @@ const fixtureDir = `${__dirname}/fixtures`;
 //
 const browser = await chromium.launch({
   // headless: false,
+  // ignoreDefaultArgs: ['--headless=old'],
+  // args: ['--headless=new']
 });
 
 async function getPage(): Promise<Page> {
-  const page = await browser.newPage({ baseURL: "http://domain" });
+  const page = await browser.newPage({
+    baseURL: "http://domain",
+  });
   await page.route("**/*", (route, request) => {
     const urlPath = new URL(request.url()).pathname;
     // const js = jsMap.get(urlPath);
@@ -58,111 +62,111 @@ async function getPage(): Promise<Page> {
 }
 
 const params: string[][] = [
-  [
-    "gs",
-    "cd",
-    "cs",
-    "cd",
-    "cs",
-    "cd",
-    "cs",
-    "cd",
-    "bs",
-    "bd",
-    "bs",
-    "bd",
-    "bs",
-    "bd",
-    "bs",
-  ],
+  // [
+  //   "gs",
+  //   "cd",
+  //   "cs",
+  //   "cd",
+  //   "cs",
+  //   "cd",
+  //   "cs",
+  //   "cd",
+  //   "bs",
+  //   "bd",
+  //   "bs",
+  //   "bd",
+  //   "bs",
+  //   "bd",
+  //   "bs",
+  // ],
 
-  ["gi_1", "cs", "bi_1"],
-  ["gi_1", "cs", "ci_2", "bs", "bi_1"],
-  ["gi_1", "cs", "ci_2", "cs", "bi_1"],
-  ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_1"],
-  ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs", "bi_1"],
-
-  [
-    "gs",
-    "cd",
-    "ci_1",
-    "cd",
-    "ci_2",
-    "cd",
-    "ci_3",
-    "cd",
-    "ci_4",
-    "bd",
-    "bd",
-    "bd",
-  ],
-  [
-    "gs",
-    "ci_1",
-    "cs",
-    "ci_2",
-    "cs",
-    "ci_3",
-    "cs",
-    "ci_4",
-    "bs",
-    "bi_1",
-    "bs",
-    "bi_1",
-  ],
-
-  ["gi_1"],
-  ["gs", "ci_1"],
-
-  ["gs", "ci_1", "cs", "ci_2"],
-  ["gs", "ci_1", "gs", "ci_2"],
-  ["gs", "ci_1", "bs"],
-  ["gs", "ci_1", "cd", "ci_2"],
-  ["gs", "ci_1", "gd", "ci_2"],
-
-  ["gd", "ci_1", "cs", "ci_2"],
-  ["gd", "ci_1", "gs", "ci_2"],
-  ["gd", "ci_1", "bd"],
-  ["gd", "ci_1", "cd", "ci_2"],
-  ["gd", "ci_1", "gd", "ci_2"],
-
-  ["gi_1", "cs", "ci_2"],
-  ["gi_1", "gs", "ci_2"],
-  ["gi_1", "cd", "ci_2"],
-  ["gi_1", "gd", "ci_2"],
-
-  ["gs", "ci_1", "gi_1"],
-  ["gi_1", "gi_1"],
-
-  ["gs", "ci_1", "cs", "ci_2", "cs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cs", "ci_2", "bs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cs", "ci_2", "cd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cs", "ci_2", "gd", "ci_3", "gi_1"],
-
-  ["gs", "ci_1", "gs", "ci_2", "cs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gs", "ci_2", "bs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gs", "ci_2", "cd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gs", "ci_2", "gd", "ci_3", "gi_1"],
-
-  ["gs", "ci_1", "bs", "ci_2", "cs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "bs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "bs", "ci_2", "bs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "bs", "ci_2", "cd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "bs", "ci_2", "gd", "ci_3", "gi_1"],
-
-  ["gs", "ci_1", "cd", "ci_2", "cs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cd", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cd", "ci_2", "bd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cd", "ci_2", "cd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cd", "ci_2", "gd", "ci_3", "gi_1"],
-
-  ["gs", "ci_1", "gd", "ci_2", "cs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gd", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gd", "ci_2", "bd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gd", "ci_2", "cd", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gd", "ci_2", "gd", "ci_3", "gi_1"],
+  ["gi_1", "cs", "bi_2"],
+  // ["gi_1", "cs", "ci_2", "bs", "bi_1"],
+  // ["gi_1", "cs", "ci_2", "cs", "bi_1"],
+  // ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_1"],
+  // ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs", "bi_1"],
+  //
+  // [
+  //   "gs",
+  //   "cd",
+  //   "ci_1",
+  //   "cd",
+  //   "ci_2",
+  //   "cd",
+  //   "ci_3",
+  //   "cd",
+  //   "ci_4",
+  //   "bd",
+  //   "bd",
+  //   "bd",
+  // ],
+  // [
+  //   "gs",
+  //   "ci_1",
+  //   "cs",
+  //   "ci_2",
+  //   "cs",
+  //   "ci_3",
+  //   "cs",
+  //   "ci_4",
+  //   "bs",
+  //   "bi_1",
+  //   "bs",
+  //   "bi_1",
+  // ],
+  //
+  // ["gi_1"],
+  // ["gs", "ci_1"],
+  //
+  // ["gs", "ci_1", "cs", "ci_2"],
+  // ["gs", "ci_1", "gs", "ci_2"],
+  // ["gs", "ci_1", "bs"],
+  // ["gs", "ci_1", "cd", "ci_2"],
+  // ["gs", "ci_1", "gd", "ci_2"],
+  //
+  // ["gd", "ci_1", "cs", "ci_2"],
+  // ["gd", "ci_1", "gs", "ci_2"],
+  // ["gd", "ci_1", "bd"],
+  // ["gd", "ci_1", "cd", "ci_2"],
+  // ["gd", "ci_1", "gd", "ci_2"],
+  //
+  // ["gi_1", "cs", "ci_2"],
+  // ["gi_1", "gs", "ci_2"],
+  // ["gi_1", "cd", "ci_2"],
+  // ["gi_1", "gd", "ci_2"],
+  //
+  // ["gs", "ci_1", "gi_1"],
+  // ["gi_1", "gi_1"],
+  //
+  // ["gs", "ci_1", "cs", "ci_2", "cs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cs", "ci_2", "gs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cs", "ci_2", "bs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cs", "ci_2", "cd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cs", "ci_2", "gd", "ci_3", "gi_1"],
+  //
+  // ["gs", "ci_1", "gs", "ci_2", "cs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gs", "ci_2", "gs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gs", "ci_2", "bs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gs", "ci_2", "cd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gs", "ci_2", "gd", "ci_3", "gi_1"],
+  //
+  // ["gs", "ci_1", "bs", "ci_2", "cs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "bs", "ci_2", "gs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "bs", "ci_2", "bs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "bs", "ci_2", "cd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "bs", "ci_2", "gd", "ci_3", "gi_1"],
+  //
+  // ["gs", "ci_1", "cd", "ci_2", "cs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cd", "ci_2", "gs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cd", "ci_2", "bd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cd", "ci_2", "cd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "cd", "ci_2", "gd", "ci_3", "gi_1"],
+  //
+  // ["gs", "ci_1", "gd", "ci_2", "cs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gd", "ci_2", "gs", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gd", "ci_2", "bd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gd", "ci_2", "cd", "ci_3", "gi_1"],
+  // ["gs", "ci_1", "gd", "ci_2", "gd", "ci_3", "gi_1"],
 ];
 
 async function handleStep(page: Page, step: string): Promise<void> {
@@ -202,6 +206,15 @@ async function handleStep(page: Page, step: string): Promise<void> {
   }
   if (step.at(0) === "b") {
     await page.goBack();
+
+    // I don't know why but this is the behavior in my browser
+    await page.evaluate(() =>
+      window.dispatchEvent(new CustomEvent("beforeunload")),
+    );
+    await page.evaluate(() =>
+      window.dispatchEvent(new CustomEvent("pageshow")),
+    );
+
     if (step.at(1) === "s") {
       await expect(page.getByTestId("main")).toHaveText("Static");
       return;
@@ -224,6 +237,8 @@ for (const steps of params) {
     const page = await getPage();
     const errors: Error[] = [];
     page.on("pageerror", (error) => errors.push(error));
+    const consoleMessages: string[] = [];
+    page.on("console", (msg) => consoleMessages.push(msg.text()));
     for (const step of steps) {
       await handleStep(page, step);
       // console.log(step);
@@ -233,6 +248,7 @@ for (const steps of params) {
       // console.log(JSON.parse(value));
     }
     expect(errors).toHaveLength(0);
+    expect(consoleMessages).toHaveLength(0);
     await page.close();
   });
 }
