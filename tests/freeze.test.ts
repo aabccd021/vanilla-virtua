e9c2b3065b4f005944f32ab35a3a61000162e31d
aabccd021 <aabccd021@gmail.com>
2025-01-17 19:28:28 +0700

    Add test for the new freeze function configuration in freeze.test.ts.

--- tests/freeze.test.ts
+++ tests/freeze.test.ts
@@ -58,6 +58,24 @@ async function getPage(): Promise<Page> {
 }
 
 const params: string[][] = [
+  [
+    "gs",
+    "cd",
+    "cs",
+    "cd",
+    "cs",
+    "cd",
+    "cs",
+    "cd",
+    "bs",
+    "bd",
+    "bs",
+    "bd",
+    "bs",
+    "bd",
+    "bs",
+  ],
+
   ["gi_1", "cs", "bi_1"],
   ["gi_1", "cs", "ci_2", "bs", "bi_1"],
   ["gi_1", "cs", "ci_2", "cs", "bi_1"],
