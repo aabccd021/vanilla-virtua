462e33867f24782b598b4eb8a830c3389397f5be
aabccd021 <aabccd021@gmail.com>
2025-01-17 15:52:35 +0700

    style: update comments and remove unused code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -63,12 +63,13 @@ type Param = {
 };
 
 const params: Param[] = [
-  { expected: "Static", steps: ["gs"] },
-  { expected: "Dynamic", steps: ["gd"] },
-  { expected: "1", steps: ["gi"] },
-  { expected: "1", steps: ["gs", "ci"] },
+  // { expected: "Static", steps: ["gs"] },
+  // { expected: "Dynamic", steps: ["gd"] },
+  // { expected: "1", steps: ["gi"] },
+  // { expected: "1", steps: ["gs", "ci"] },
+  // { expected: "2", steps: ["gs", "ci", "cs", "ci"] },
+  { expected: "2", steps: ["gs", "ci", "gs", "ci"] },
   // { expected: "2", steps: ["gs", "ci", "gb", "ci"] },
-  { expected: "2", steps: ["gs", "ci", "cs", "ci"] },
 ];
 
 for (const param of params) {
@@ -93,12 +94,12 @@ for (const param of params) {
       } else {
         throw new Error(`Absurd: ${step}`);
       }
-      // console.log(step);
-      // const value = await page.evaluate(() =>
-      //   sessionStorage.getItem("freeze-cache"),
-      // );
-      // const v = JSON.parse(value);
-      // console.log(v);
+      console.log(step);
+      const value = await page.evaluate(() =>
+        sessionStorage.getItem("freeze-cache"),
+      );
+      const v = JSON.parse(value);
+      console.log(v);
     }
     expect(await page.getByTestId("main").textContent()).toBe(param.expected);
     await page.close();
