8de26c1430b459ed532896b0cf697f824e0e325d
aabccd021 <aabccd021@gmail.com>
2025-01-17 15:53:16 +0700

    fix: uncomment console.logs and sessionStorage code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -63,11 +63,11 @@ type Param = {
 };
 
 const params: Param[] = [
-  // { expected: "Static", steps: ["gs"] },
-  // { expected: "Dynamic", steps: ["gd"] },
-  // { expected: "1", steps: ["gi"] },
-  // { expected: "1", steps: ["gs", "ci"] },
-  // { expected: "2", steps: ["gs", "ci", "cs", "ci"] },
+  { expected: "Static", steps: ["gs"] },
+  { expected: "Dynamic", steps: ["gd"] },
+  { expected: "1", steps: ["gi"] },
+  { expected: "1", steps: ["gs", "ci"] },
+  { expected: "2", steps: ["gs", "ci", "cs", "ci"] },
   { expected: "2", steps: ["gs", "ci", "gs", "ci"] },
   // { expected: "2", steps: ["gs", "ci", "gb", "ci"] },
 ];
@@ -94,12 +94,12 @@ for (const param of params) {
       } else {
         throw new Error(`Absurd: ${step}`);
       }
-      console.log(step);
-      const value = await page.evaluate(() =>
-        sessionStorage.getItem("freeze-cache"),
-      );
-      const v = JSON.parse(value);
-      console.log(v);
+      // console.log(step);
+      // const value = await page.evaluate(() =>
+      //   sessionStorage.getItem("freeze-cache"),
+      // );
+      // const v = JSON.parse(value);
+      // console.log(v);
     }
     expect(await page.getByTestId("main").textContent()).toBe(param.expected);
     await page.close();
