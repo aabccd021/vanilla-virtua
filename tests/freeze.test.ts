210bb9f3aac638ccee6fd9de782d8b06483b5aff
aabccd021 <aabccd021@gmail.com>
2025-01-17 19:25:16 +0700

    feat: Add new test cases for freezing functionality

--- tests/freeze.test.ts
+++ tests/freeze.test.ts
@@ -59,6 +59,8 @@ async function getPage(): Promise<Page> {
 
 const params: string[][] = [
   ["gi_1", "cs", "bi_1"],
+  ["gi_1", "cs", "ci_2", "bs", "bi_1"],
+  ["gi_1", "cs", "ci_2", "cs", "bi_1"],
 
   [
     "gs",
