be5d90448cd450e0762a2a440909e4269c645bac
aabccd021 <aabccd021@gmail.com>
2025-01-17 17:01:47 +0700

    Delete unnecessary files for cleanup.

--- src/ra.ts
+++
@@ -1,4 +0,0 @@
-const newChild = window.document.createElement("h2");
-newChild.textContent = "this is dynamically added";
-newChild.dataset["testid"] = "main";
-window.document.body.appendChild(newChild);
