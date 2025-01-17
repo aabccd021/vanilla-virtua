8ac3b45b800e96de6a10de1ef71fa77a27938f7d
aabccd021 <aabccd021@gmail.com>
2025-01-17 17:24:47 +0700

    Update test case for freezing function.

--- tests/freeze.test.ts
+++ tests/freeze.test.ts
@@ -84,7 +84,7 @@ const params: Param[] = [
   { expected: "1", steps: ["gs", "ci", "re"] },
 
   { expected: "3", steps: ["gs", "ci", "cs", "ci", "cs", "ci"] },
-  // { expected: "3", steps: ["gs", "ci", "cs", "ci", "gs", "ci"] },
+  { expected: "3", steps: ["gs", "ci", "cs", "ci", "gs", "ci"] },
   { expected: "3", steps: ["gs", "ci", "cs", "ci", "gb", "ci"] },
   { expected: "3", steps: ["gs", "ci", "cs", "ci", "cd", "ci"] },
   { expected: "3", steps: ["gs", "ci", "cs", "ci", "gd", "ci"] },
