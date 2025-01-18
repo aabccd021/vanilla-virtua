5b4824e28314725a68b719a51a762da270d6c119
aabccd021 <aabccd021@gmail.com>
2025-01-18 16:00:25 +0700

    feat: Add redundant test for array comparison

--- tests/common.ts
+++ tests/common.ts
@@ -1,26 +1,36 @@
+function testRedundant(arr1: string[], arr2: string[]): void {
+  const smaller = arr1.length < arr2.length ? arr1 : arr2;
+  for (let i = 0; i < smaller.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue;
+    } else {
+      return;
+    }
+  }
+  if (arr1.length === arr2.length) {
+    return;
+  }
+  const arr1Str = arr1.map((s) => `"${s}"`).join(", ");
+  const arr2Str = arr2.map((s) => `"${s}"`).join(", ");
+  const smallerStr = arr1.length < arr2.length ? arr1Str : arr2Str;
+  const largerStr = arr1.length < arr2.length ? arr2Str : arr1Str;
+  throw new Error(`redundant: [${smallerStr}] in [${largerStr}]`);
+}
+
 export const params: string[][] = [
-  ["gd", "ci_1", "bd"],
   ["gd", "ci_1", "cd", "ci_2"],
   ["gd", "ci_1", "cs", "ci_2"],
   ["gd", "ci_1", "gd", "ci_2"],
   ["gd", "ci_1", "gs", "ci_2"],
-  ["gd", "ci_1"],
-  ["gd"],
   ["gi_1", "cd", "ci_2"],
   ["gi_1", "cs", "bi_2"],
   ["gi_1", "cs", "ci_2", "bs", "bi_3"],
   ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_4"],
-  ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs"],
-  ["gi_1", "cs", "ci_2", "bs"],
   ["gi_1", "cs", "ci_2", "cs", "bi_3"],
   ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs", "bi_4"],
-  ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs"],
-  ["gi_1", "cs", "ci_2", "cs"],
-  ["gi_1", "cs", "ci_2"],
   ["gi_1", "gd", "ci_2"],
   ["gi_1", "gi_1"],
   ["gi_1", "gs", "ci_2"],
-  ["gi_1"],
   [
     "gs",
     "cd",
@@ -57,14 +67,11 @@ export const params: string[][] = [
   ["gs", "ci_1", "bs", "ci_2", "cs", "ci_3", "gi_1"],
   ["gs", "ci_1", "bs", "ci_2", "gd", "ci_3", "gi_1"],
   ["gs", "ci_1", "bs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "bs", "ci_2"],
-  ["gs", "ci_1", "bs"],
   ["gs", "ci_1", "cd", "ci_2", "bd", "ci_3", "gi_1"],
   ["gs", "ci_1", "cd", "ci_2", "cd", "ci_3", "gi_1"],
   ["gs", "ci_1", "cd", "ci_2", "cs", "ci_3", "gi_1"],
   ["gs", "ci_1", "cd", "ci_2", "gd", "ci_3", "gi_1"],
   ["gs", "ci_1", "cd", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cd", "ci_2"],
   ["gs", "ci_1", "cs", "ci_2", "bs", "ci_3", "gi_1"],
   ["gs", "ci_1", "cs", "ci_2", "cd", "ci_3", "gi_1"],
   [
@@ -82,24 +89,24 @@ export const params: string[][] = [
     "bi_6",
     "bi_6",
   ],
-  ["gs", "ci_1", "cs", "ci_2", "cs", "ci_3", "cs", "ci_4", "bs"],
   ["gs", "ci_1", "cs", "ci_2", "cs", "ci_3", "gi_1"],
   ["gs", "ci_1", "cs", "ci_2", "gd", "ci_3", "gi_1"],
   ["gs", "ci_1", "cs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "cs", "ci_2"],
   ["gs", "ci_1", "gd", "ci_2", "bd", "ci_3", "gi_1"],
   ["gs", "ci_1", "gd", "ci_2", "cd", "ci_3", "gi_1"],
   ["gs", "ci_1", "gd", "ci_2", "cs", "ci_3", "gi_1"],
   ["gs", "ci_1", "gd", "ci_2", "gd", "ci_3", "gi_1"],
   ["gs", "ci_1", "gd", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gd", "ci_2"],
   ["gs", "ci_1", "gi_1"],
   ["gs", "ci_1", "gs", "ci_2", "bs", "ci_3", "gi_1"],
   ["gs", "ci_1", "gs", "ci_2", "cd", "ci_3", "gi_1"],
   ["gs", "ci_1", "gs", "ci_2", "cs", "ci_3", "gi_1"],
   ["gs", "ci_1", "gs", "ci_2", "gd", "ci_3", "gi_1"],
   ["gs", "ci_1", "gs", "ci_2", "gs", "ci_3", "gi_1"],
-  ["gs", "ci_1", "gs", "ci_2"],
-  ["gs", "ci_1"],
-  ["gs"],
 ];
+
+for (const arr1 of params) {
+  for (const arr2 of params) {
+    testRedundant(arr1, arr2);
+  }
+}
