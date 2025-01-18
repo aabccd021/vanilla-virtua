7921de6442a70b6d94e4035d988ee0dfb3652c41
aabccd021 <aabccd021@gmail.com>
2025-01-18 14:08:21 +0700

    chore: update test data for no-bfcache functionality

--- tests/no-bfcache.ts
+++ tests/no-bfcache.ts
@@ -3,5 +3,5 @@ export const params: string[][] = [
   ["gi_1", "cs", "ci_2", "bs", "bi_1"],
   ["gi_1", "cs", "ci_2", "cs", "bi_1"],
   ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_1"],
-  // ["gi_1", "cs", "ci_2", "cs", "ci_1", "cs", "bi_2"],
+  ["gi_1", "cs", "ci_2", "cs", "ci_3", "cs", "bi_1"],
 ];
