a4c86c7c505947841833289d26cd3932092de735
aabccd021 <aabccd021@gmail.com>
2025-01-18 14:07:24 +0700

    Refactor params structure in tests: Add new array for bfcache and update existing one.

--- tests/bfcache.ts
+++ tests/bfcache.ts
@@ -1,6 +1,5 @@
 export const params: string[][] = [
   ["gi_1", "cs", "bi_2"],
-
   ["gi_1", "cs", "ci_2", "bs", "bi_2"],
   ["gi_1", "cs", "ci_2", "cs", "bi_3"],
   ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_2"],
--- tests/no-bfcache.ts
+++ tests/no-bfcache.ts
@@ -2,4 +2,6 @@ export const params: string[][] = [
   ["gi_1", "cs", "bi_1"],
   ["gi_1", "cs", "ci_2", "bs", "bi_1"],
   ["gi_1", "cs", "ci_2", "cs", "bi_1"],
+  ["gi_1", "cs", "ci_2", "bs", "ci_3", "bs", "bi_1"],
+  // ["gi_1", "cs", "ci_2", "cs", "ci_1", "cs", "bi_2"],
 ];
