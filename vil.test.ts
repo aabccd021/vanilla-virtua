34b87cd7d6f21b4e8f9ed2d5a9f99a703559d5b9
aabccd021 <aabccd021@gmail.com>
2025-01-24 14:32:39 +0700

    - Update biome.jsonc with new config options and refactor vil.test.ts for scroll testing.

--- vil.test.ts
+++ vil.test.ts
@@ -1,6 +1,48 @@
-import { expect, test } from "@playwright/test";
+import { type Locator, type Page, expect, test } from "@playwright/test";
 
-test("Item 0 in viewport", async ({ page }) => {
+const getScrollable = async (page: Page): Promise<Locator> => {
+  const locator = page.locator(
+    '*[style*="overflow-y: auto"],*[style*="overflow-y:auto"],*[style*="overflow-x: auto"],*[style*="overflow-x:auto"],*[style*="overflow: auto"],*[style*="overflow:auto"]',
+  );
+  await locator.waitFor();
+  return locator;
+};
+
+const scrollToBottom = (scrollable: Locator): Promise<void> => {
+  return scrollable.evaluate((e) => {
+    return new Promise<void>((resolve) => {
+      let timer: ReturnType<typeof setTimeout> | null = null;
+
+      const onScroll = (): void => {
+        e.scrollTop = e.scrollHeight;
+
+        if (timer !== null) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          if (e.scrollTop + (e as HTMLElement).offsetHeight >= e.scrollHeight) {
+            e.removeEventListener("scroll", onScroll);
+            resolve();
+          } else {
+            onScroll();
+          }
+        }, 50);
+      };
+      e.addEventListener("scroll", onScroll);
+
+      onScroll();
+    });
+  });
+};
+
+test("Item 0 shown", async ({ page }) => {
   await page.goto("/page1.html");
   await expect(page.getByText("Item 0")).toBeInViewport();
+});
+
+test("Item 29 shown after scroll", async ({ page }) => {
+  await page.goto("/page1.html");
+  const scrollable = await getScrollable(page);
+  await scrollToBottom(scrollable);
+  await expect(page.getByText("Item 29")).toBeInViewport();
 });
