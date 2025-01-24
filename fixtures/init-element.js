653e627020319e9c9ce8987e0c727c47e31df9e9
aabccd021 <aabccd021@gmail.com>
2025-01-24 20:07:03 +0700

    chore: Update type definitions and enable 'checkJs' in tsconfig

--- fixtures/init-element.js
+++ fixtures/init-element.js
@@ -1,15 +1,25 @@
 /**
- * @typedef {Object} VilInitEvent
- * @property {Element} element
- * @property {string} listId
+ * Type representing an unsubscribe function.
+ * @typedef {Function} Unsub
+ * @returns {void}
  */
 
 /**
- * @param {VilInitEvent} e
- * @return {{function(): void | void}}
+ * Type representing the initialization event for a virtualized list.
+ * @typedef {Object} VilInitEvent
+ * @property {Element} element - The DOM element associated with the event.
+ * @property {string} listId - The ID of the list being initialized.
+ */
+
+/**
+ * Type representing a function that initializes a child component.
+ * @typedef {Function} InitChild
+ * @param {VilInitEvent} event - The initialization event.
+ * @returns {Promise<Unsub | undefined> | Unsub | undefined} - A promise that resolves to an unsubscribe function or undefined.
  */
 export function vilInitChild({ listId, element }) {
-  if (listId !== "main-list") {
+  // assert element is HTMLElement
+  if (listId !== "main-list" || !(element instanceof HTMLElement)) {
     return;
   }
   element.addEventListener("click", () => {
