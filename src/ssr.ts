8e11a0ae82a380ce19913c7e5b265bfc88ef81ce
aabccd021 <aabccd021@gmail.com>
2025-01-27 17:14:35 +0700

    refactor(ssr): Add ability to dynamically append rows

--- src/ssr.ts
+++ src/ssr.ts
@@ -1,4 +1,18 @@
-import { init } from "./index.ts";
+import { init, appendChildren } from "./index.ts";
 
 const root = document.getElementById("root")!;
-init({ root });
+const vlist = init({ root });
+
+const heights = [20, 40, 80, 77];
+
+document.getElementById("append-button")?.addEventListener("click", () => {
+	const newRows = Array.from({ length: 30 }).map((_, i) => {
+		const item = document.createElement("div");
+		const height = heights[i % 4];
+		item.style.height = `${height}px`;
+		item.style.borderBottom = "solid 1px #ccc";
+		item.textContent = `Height: ${height}px`;
+		return item;
+	});
+	appendChildren(vlist.context, newRows);
+});
