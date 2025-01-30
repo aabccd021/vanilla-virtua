5020f72dde0ddfcb765c8bf7aae659ab61d3000e
aabccd021 <aabccd021@gmail.com>
2025-01-30 14:47:33 +0700

    1. Refactor: Update README and dependencies 2. Fix: Adjust document element selection 3. Chore: Organize formatting and packages 4. Style: Update root div tag in HTML

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ npm run dev
 # Example Usage
 
 [main.ts](./src/main.ts)
+
 ```ts
 import { appendChildren, init } from "./index.ts";
 
 const heights = [20, 40, 80, 77];
 
-const createRows = () => 
+const createRows = () =>
   Array.from({ length: 30 }).map((_, i) => {
     const item = document.createElement("div");
     const height = heights[i % 4];
@@ -32,15 +33,11 @@ const initialRows = createRows();
 const vlist = init({ children: initialRows });
 
 // Mount list root
-document
-  .getElementById("app")!
-  .appendChild(vlist.root);
+document.getElementById("app")!.appendChild(vlist.root);
 
 // Append 30 items when button is clicked
-document
-  .getElementById("append-button")!
-  .addEventListener("click", () => {
-    const newRows = createRows();
-    appendChildren(vlist.context, newRows);
-  })
+document.getElementById("append-button")!.addEventListener("click", () => {
+  const newRows = createRows();
+  appendChildren(vlist.context, newRows);
+});
 ```
--- flake.nix
+++ flake.nix
@@ -1,16 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, treefmt-nix }:
 
     let
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.prettier.enable = true;
+        programs.nixpkgs-fmt.enable = true;
+        programs.biome.enable = true;
+        programs.shfmt.enable = true;
+        settings.formatter.prettier.priority = 1;
+        settings.formatter.biome.priority = 2;
+        settings.global.excludes = [ "LICENSE" "*.ico" ];
+      };
+
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      packages = {
+        formatting = treefmtEval.config.build.check self;
+      };
+
+      gcroot = packages // {
+        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+      };
 
     in
 
     {
+
+      packages.x86_64-linux = gcroot;
+      checks.x86_64-linux = gcroot;
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
 
       devShells.x86_64-linux.default = pkgs.mkShellNoCC {
         buildInputs = [
