5b5bb2a4fc63e5ab501b1b87caaccfef460747de
aabccd021 <aabccd021@gmail.com>
2025-01-30 20:59:05 +0700

    chore: refactor playwright.config.ts - Update reuseExistingServer value

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,6 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   maxFailures: 1,
-  timeout: 15000,
-  expect: {
-    timeout: 15000,
-  },
   projects: [
     {
       name: "chromium",
@@ -27,7 +23,7 @@ export default defineConfig({
   webServer: {
     command: "npm run serve",
     url: "http://127.0.0.1:8000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
     stderr: "ignore",
   },
 });
