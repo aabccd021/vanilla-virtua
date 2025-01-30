1aa40c6e2ca9fd0a4377201d7e3e71ad2a0e2378
aabccd021 <aabccd021@gmail.com>
2025-01-30 20:11:17 +0700

    Empty

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,35 +6,13 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
-const IOS_SPECS = "iOS";
+const _IOS_SPECS = "iOS";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: "./e2e",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "list",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-
-    // video: "retain-on-failure"
-  },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
@@ -45,26 +23,13 @@ export default defineConfig({
           args: ["--headless"],
         },
       },
-      grepInvert: new RegExp(String.raw`${IOS_SPECS}`),
     },
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-      grepInvert: new RegExp(String.raw`${IOS_SPECS}`),
     },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    //   grepInvert: new RegExp(String.raw`${IOS_SPECS}`),
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 13"] },
-    //   grep: new RegExp(String.raw`${IOS_SPECS}`),
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: "esbuild stories/*.ts --bundle --outdir=stories --servedir=stories",
     url: "http://127.0.0.1:8000",
