ab5c3f731f1d373896f133f46c44e98916f438ce
aabccd021 <aabccd021@gmail.com>
2025-01-30 14:37:01 +0700

    feat: Add vList component and default story for vertical list display

---
+++ src/vList.ts
@@ -0,0 +1,29 @@
+import { init as indexInit } from "./index.ts";
+
+type InitResult = {
+  readonly root: HTMLElement;
+}
+
+export function init({
+  children,
+  style,
+}: {
+  readonly children?: HTMLElement[];
+  readonly style?: Partial<CSSStyleDeclaration>;
+}): InitResult {
+  const container = document.createElement("div");
+  for (const child of children ?? []) {
+    container.appendChild(child);
+  }
+
+  const root = document.createElement("div");
+  root.appendChild(container);
+
+  if (style !== undefined) {
+    Object.assign(root.style, style);
+  }
+
+  indexInit({ container });
+
+  return { root };
+}
---
+++ stories/vlist-default.js
@@ -0,0 +1,17 @@
+import { init as vListInit } from "../src/vList.ts";
+
+const createRows = (num) => {
+  const heights = [20, 40, 80, 77];
+  return Array.from({ length: num }).map((_, i) => {
+    const div = document.createElement("div");
+    div.style.height = `${heights[i % 4]}px`;
+    div.style.borderBottom = "solid 1px #ccc";
+    div.style.background = "#fff";
+    div.textContent = `${i}`;
+    return div;
+  });
+};
+
+const vList = vListInit({ style:{height: "100vh"}, children: createRows(1000) });
+
+document.body.appendChild(vList.root);
